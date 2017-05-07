175fe190501d9735d9fa68d456d4bc2e85c27493
Mohit Gupta <gupta.moh@husky.neu.edu>
2017-05-06 20:27:19 -0400

    concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 var wg sync.WaitGroup
+var counter int64
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 func main() {
 	wg.Add(2)
-	go foo()
-	go bar()
+	go incrementor("Foo")
+	go incrementor("Bar")
 	wg.Wait()
+	fmt.Println("Final Counter:", counter)
 }
 
 func foo() {
 
 	for i := 0; i < 100; i++ {
 		fmt.Println("Foo :", i)
-		time.Sleep(time.Duration( 3 * time.Millisecond))
+		time.Sleep(time.Duration(3 * time.Millisecond))
 	}
 	wg.Done()
 }
@@ -28,7 +36,21 @@ func bar() {
 
 	for i := 0; i < 100; i++ {
 		fmt.Println("Bar :", i)
-		time.Sleep(time.Duration( 3 * time.Millisecond))
+		time.Sleep(time.Duration(3 * time.Millisecond))
+	}
+
+	wg.Done()
+
+}
+
+func incrementor(s string) {
+
+	for i := 0; i < 20; i++ {
+
+		time.Sleep(time.Duration(3 * time.Millisecond))
+		atomic.AddInt64(&counter, 1)
+		fmt.Println(s, ":", i, ",", counter)
+
 	}
 
 	wg.Done()
